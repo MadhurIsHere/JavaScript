279a651cc07c7c1aaba538f76572b329cda1fe8e
Madhur Rastogi <madhurrastogi20@gmail.com>
2025-12-30 21:06:06 +0000

    8 videoss done

---
+++ Basics/03_operations.js
@@ -0,0 +1,6 @@
+console.log(2 + "1");
+console.log(1 + "2");
+console.log("1" + 1 + 1 + 1);  // treated as string 
+console.log(1+"1"+1+1); // treated as string
+console.log(1+1+"1"+1); // first add 1+1=2 then treated rest as string
+
