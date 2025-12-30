89de43ad1e3db4eff6e7374a311afeca265c1cd5
Madhur Rastogi <madhurrastogi20@gmail.com>
2025-12-30 21:21:15 +0000

    "DataTypes summary"

--- Basics/02_dataTypes.js
+++ Basics/02_dataTypes.js
@@ -1,8 +1,10 @@
 "use strict";
+
+
 // Data Types in JavaScript
 
 // 1. Primitive Data Types
-
+// Number, String ,Boolean , Null ,undefined, Symbol, BigInt
 // a) Number
 const age = 25;
 const price = 99.99;
@@ -23,12 +25,33 @@ let address = null; // explicitly set to null
 
 // f) Symbol
 const uniqueId = Symbol('id');
+const id=Symbol('123')
+const newId=Symbol('123');
+console.log(id===newId);
+console.log(id);
+console.log(newId);
 
 // g) BigInt
 const bigNumber = BigInt(9007199254740991);
 
-// 2. Non-Primitive Data Types
+// 2. Non-Primitive Data Types / Reference Type
 
 // a) Object
 console.log(typeof address) // null is of object type in js
 console.log(typeof middleName) // undefined is of undefined type in js
+let myObj={
+    name:"Madhur",
+    age:20,
+    gender:"Male",
+}
+console.log(myObj);
+
+// b) Arrays
+const heros=["Iron Man","Spider Man","Super Man"];
+console.log(heros);
+
+
+// c) Functions
+const myFun=function(){
+    console.log("Hello World");
+}
