94e09b2b15f28c2333ae4bb24cd6a868b29606a6
Madhur Rastogi <madhurrastogi20@gmail.com>
2025-12-30 21:06:24 +0000

    done 8

--- Basics/03_operations.js
+++ Basics/03_operations.js
@@ -4,3 +4,6 @@ console.log("1" + 1 + 1 + 1);  // treated as string
 console.log(1+"1"+1+1); // treated as string
 console.log(1+1+"1"+1); // first add 1+1=2 then treated rest as string
 
+//************************************Comparison Operators*************************************/
+console.table([null == 0,null === 0,null > 0,null < 0," ",undefined == 0,undefined === 0,undefined >= 0,undefined <= 0,undefined> 0,undefined < 0]);
+console.table([null >= 0,null <= 0]);
